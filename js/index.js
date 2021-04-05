b6000a3bfe047381966263c8bdc3d0131d9e9819
samuelbatista3rios <66880647+samuelbatista3rios@users.noreply.github.com>
2021-04-05 16:56:27 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// javascripts/intro.js
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+const player = new GameObject
+window.onload = () => {
+    document.getElementById("start-button").onclick = ()
+}
